620ebda7acebd35959e32f7065efbfc10d91cbce
stropee <stropee@campus-eni.fr>
2024-01-30 16:53:23 +0100

    TP 3

--- app/src/main/java/com/example/enishop/MainActivity.kt
+++ app/src/main/java/com/example/enishop/MainActivity.kt
@@ -2,17 +2,17 @@ package com.example.enishop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.enishop.repository.ArticleRepository
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         val article = ArticleRepository.getArticle(2)
-        println(article)
-
+        Log.i(TAG, article.toString())
 
     }
 }
